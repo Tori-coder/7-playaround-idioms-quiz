73848bfa77a97bc36397386a92c42f446a97f230
Tori-coder <114353884+Tori-coder@users.noreply.github.com>
2024-04-21 01:23:53 +0300

    looks better now

--- assets/js/idioms.js
+++ assets/js/idioms.js
@@ -2,17 +2,17 @@
 const idiomsArray = [
     {
       "idiom": "A piece of cake",
-      "meanings": ["Something very easy to do", "A delicious dessert", "A small part of something larger"],
+      "meanings": ["A delicious dessert", "Something very easy to do", "A small part of something larger"],
       "answer": "Something very easy to do"
     },
     {
       "idiom": "Bite the bullet",
-      "meanings": ["To endure a painful experience with courage", "To express anger", "To bite on something hard"],
+      "meanings": ["To express anger", "To bite on something hard", "To endure a painful experience with courage"],
       "answer": "To endure a painful experience with courage"
     },
     {
       "idiom": "Cost an arm and a leg",
-      "meanings": ["To be very expensive", "To be free", "To require a significant sacrifice"],
+      "meanings": ["To be free", "To require a significant sacrifice", "To be very expensive"],
       "answer": "To be very expensive"
     },
     {
@@ -22,22 +22,22 @@ const idiomsArray = [
     },
     {
       "idiom": "Jump on the bandwagon",
-      "meanings": ["To join a popular trend or activity", "To criticize others", "To jump to conclusions"],
+      "meanings": ["To criticise others", "To join a popular trend or activity", "To jump to conclusions"],
       "answer": "To join a popular trend or activity"
     },
     {
       "idiom": "Bite off more than you can chew",
-      "meanings": ["To take on a task that is way too big", "To eat too quickly", "To bite on something hard"],
+      "meanings": ["To eat too quickly", "To take on a task that is way too big", "To bite on something hard"],
       "answer": "To take on a task that is way too big"
     },
     {
       "idiom": "Break the ice",
-      "meanings": ["To start a conversation in a social situation", "To cause trouble", "To break something frozen"],
+      "meanings": ["To break something frozen", "To start a conversation in a social situation", "To cause trouble"],
       "answer": "To start a conversation in a social situation"
     },
     {
       "idiom": "Burn the midnight oil",
-      "meanings": ["To work late into the night", "To relax in the evening", "To set something on fire"],
+      "meanings": ["To relax in the evening", "To work late into the night", "To set something on fire"],
       "answer": "To work late into the night"
     },
     {
@@ -47,22 +47,22 @@ const idiomsArray = [
     },
     {
       "idiom": "Don't cry over spilled milk",
-      "meanings": ["Don't worry about things that have already happened", "Cry for help", "To spill liquid accidentally"],
+      "meanings": ["Cry for help", "To spill liquid accidentally", "Don't worry about things that have already happened"],
       "answer": "Don't worry about things that have already happened"
     },
     {
       "idiom": "Drive someone up the wall",
-      "meanings": ["To annoy someone greatly", "To make someone happy", "To drive recklessly"],
+      "meanings": ["To make someone happy", "To annoy someone greatly", "To drive recklessly"],
       "answer": "To annoy someone greatly"
     },
     {
       "idiom": "Hit the sack",
-      "meanings": ["To go to bed", "To hit someone with a sack", "To hit something hard"],
+      "meanings": ["To hit something hard", "To go to bed", "To hit someone with a sack"],
       "answer": "To go to bed"
     },
     {
       "idiom": "Let the cat out of the bag",
-      "meanings": ["To reveal a secret", "To buy a cat", "To let an animal out of a bag"],
+      "meanings": ["To buy a cat", "To let an animal out of a bag", "To reveal a secret"],
       "answer": "To reveal a secret"
     },
     {
@@ -72,33 +72,23 @@ const idiomsArray = [
     },
     {
       "idiom": "Once in a blue moon",
-      "meanings": ["Very rarely", "Every day", "During a lunar eclipse"],
+      "meanings": ["During a lunar eclipse", "Very rarely", "A long time ago"],
       "answer": "Very rarely"
     },
     {
-      "idiom": "Piece of cake",
-      "meanings": ["Something very easy to do", "A delicious dessert", "A small part of something larger"],
-      "answer": "Something very easy to do"
-    },
-    {
       "idiom": "The ball is in your court",
-      "meanings": ["It's your turn to make a decision", "The ball is lost", "A game involving balls"],
+      "meanings": ["The ball is lost", "It's your turn to make a decision", "A game involving balls"],
       "answer": "It's your turn to make a decision"
     },
     {
       "idiom": "Under the weather",
-      "meanings": ["Feeling ill or sick", "Feeling happy", "Underneath weather-related conditions"],
+      "meanings": ["Excessively cold", "Ill or sick", "Under weather-related conditions"],
       "answer": "Feeling ill or sick"
     },
     {
       "idiom": "Break a leg",
-      "meanings": ["Good luck", "Get well soon", "To fracture a bone"],
+      "meanings": ["Get well soon", "Good luck", "It's an insult"],
       "answer": "Good luck"
-    },
-    {
-      "idiom": "Cost an arm and a leg",
-      "meanings": ["To be very expensive", "To be free", "To require a significant sacrifice"],
-      "answer": "To be very expensive"
     },
     {
       "idiom": "Jump the gun",
@@ -111,25 +101,21 @@ const idiomsArray = [
       "answer": "To die"
     },
     {
-      "idiom": "Once in a blue moon",
-      "meanings": ["Very rarely", "Every day", "During a lunar eclipse"],
+      "idiom": "It's not my cup of tea",
+      "meanings": ["It's not a nice drink", "I don't like it", "I don't take responsibility"],
       "answer": "Very rarely"
     },
     {
       "idiom": "Pull someone's leg",
-      "meanings": ["To joke with someone", "To push someone", "To pull someone's body part"],
+      "meanings": ["To push someone", "To joke with someone", "To pull someone's body part"],
       "answer": "To joke with someone"
     },
     {
       "idiom": "Spill the beans",
-      "meanings": ["To reveal a secret", "To clean up a mess", "To spill food"],
+      "meanings": ["To clean up a mess", "To spill food", "To reveal a secret"],
       "answer": "To reveal a secret"
     },
-    {
-      "idiom": "Time flies",
-      "meanings": ["Time passes quickly", "Time is frozen", "To fly an aircraft"],
-      "answer": "Time passes quickly"
-    }
+    
   ];
   
   
